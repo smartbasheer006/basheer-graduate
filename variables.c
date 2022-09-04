8fb8bee0461736a62e25d204cc380439ce89af74
smartbasheer006 <112753286+smartbasheer006@users.noreply.github.com>
2022-09-04 01:45:59 -0700

    variable

---
+++ variables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    printf("welcome to c program...");
+    int age =19;
+    char name[5]="basheer";
+    printf("\nMy name is...%s",name);
+    printf("\nMy age is...%d",age);
+    char c='a';
+    char cc=100;
+    const double pi=3.14;
+    //pi=50;
+    printf("\tMy pi value is...:%if",pi);
+    printf("\n the size of pi variable is...%d",sizeof(pi));
+
+}
