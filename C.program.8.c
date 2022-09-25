fd10bb083932fd2333efd2cb9edb719f76a8d65a
smartbasheer006 <112753286+smartbasheer006@users.noreply.github.com>
2022-09-25 00:01:31 -0700

    Add files via upload

---
+++ C.program.8.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b;
+    printf("enter the integer and then a float:");
+    scanf("%d%f",&a,&b);
+    printf("you enter %d and %f",a,b);
+    return 0;
+}
