b6f9972e0379f808d6cbf7b56b39ed9779f92563
smartbasheer006 <112753286+smartbasheer006@users.noreply.github.com>
2022-09-24 23:18:34 -0700

    input for float double

---
+++ Cprogram.5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+    float num1;
+    double num2;
+    printf("enter the number:");
+    scanf("%f",&num1);
+    printf("enter the another number:");
+    scanf("if",num2);
+    printf("num1=%f\n",num1);
+    printf("num2=%if\n",num2);
+    return 0;
+
+}
