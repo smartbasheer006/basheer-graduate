1594e5dd1f83fa654a293b31cb90b59f55436e81
smartbasheer006 <112753286+smartbasheer006@users.noreply.github.com>
2022-09-25 00:02:41 -0700

    input for integer

---
+++ C.program.9.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+         {
+          int a;
+          printf("enter an integer\n");
+          scanf("%d",a);
+          printf("integer that you have enter is S%d\n",a);
+          return 0;
+         }
