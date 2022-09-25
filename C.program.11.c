d89adb2f50b0343306c555e5d14c7e6f8d5542f2
smartbasheer006 <112753286+smartbasheer006@users.noreply.github.com>
2022-09-25 00:30:13 -0700

    add two number

---
+++ C.program.11.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the two number to add\n");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("sum of enter two number%d\n",c);
+    return 0;
+}
