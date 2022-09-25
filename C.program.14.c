6083789d9fb02ecce5931144327d2a88589a6873
smartbasheer006 <112753286+smartbasheer006@users.noreply.github.com>
2022-09-25 01:15:30 -0700

    assignment operator

---
+++ C.program.14.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a=5,c;
+    c = a;
+    printf("c=%d\n",c);
+    c+=a;
+    printf("c=%d\n",c);
+    c-=a;
+    printf("c=%d\n",c);
+    c*=a;
+    printf("c=%d\n",c);
+    c/=a;
+    printf("c=%d\n",c);
+    c%=a;
+    printf("c=%d",c);
+    return 0;
+
+}
