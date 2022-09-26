6148a186ecf87e411e98760c6ed666e81b88a097
smartbasheer006 <112753286+smartbasheer006@users.noreply.github.com>
2022-09-26 03:32:36 -0700

    Add files via upload

---
+++ smartgraduate/Printaninteger.java
@@ -0,0 +1,13 @@
+package smartgraduate;
+
+import java.util.Scanner;
+
+public class Printaninteger {
+	public static void main(String[] args) {
+		Scanner reader=new Scanner(System.in);
+		System.out.println("enter a number:");
+		int number =reader.nextInt();
+		System.out.println("you enter:"+number);
+	}
+}
+	
